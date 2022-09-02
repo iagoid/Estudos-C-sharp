2f043a99c3cbd709211503cb41c1c6e5c8bb04d6
iagoid <iagoid01@gmail.com>
2022-09-02 14:40:04 -0300

    first commit

---
+++ Basico do Forms/Projeto1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* O Design do formulário é todo feito no arquivo Form1.Designer.cs
+ * Nas references temos as referências para usar no projeto
+ * E as properties que são as propriedade do projeto(nome, versões, etc)
+ * 
+ * Para compilar menu build->BuildSolution, ele é salvo dentro da pasta bin do projeto
+ * em uma pasta debug ou release(dependendo do tipo de compilação)
+*/
+
+
+// Namespace usado para ser incorporado e posteriormente referenciado
+// no código fonte do projeto
+namespace Projeto1 
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int teste = 10;
+            Console.WriteLine(teste);
+           
+        }
+    }
+}
---
+++ Basico/Basico/Program.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basico
+{
+    internal class Program
+    {
+        // Enums
+        // Enumera automaticamente começando do 0+
+        enum Notas
+        {
+            Num1 = 1,
+            Num2,
+            Num3,
+            Num4,
+            Num5,
+        }
+        Notas notas = Notas.Num1;
+
+
+        // Struct
+        struct Pessoa
+        {
+            public string nome;
+            public int idade;
+            private float altura;
+        }
+        
+
+        static void Main(string[] args)
+        {
+            #region Tipos de dados
+            // Inteiro com sinal
+            sbyte num1 = 127;
+            short num2 = 32767;
+            int num3 = 2_147_483_647;
+            long num4 = 9_223_372_036_854_775_807;
+
+            // Inteiro sem sinal(positivo)
+            byte num5 = 255;
+            ushort num6 = 65_535;
+            uint num7 = 4_294_967_295;
+            ulong num8 = 18_446_744_073_709_551_615;
+
+            num1++; // Se eu adicionar um numero além do limite ele 
+            Console.WriteLine(num1);
+
+            // Ponto flutuante
+            float real1 = 3.402823e38f; //32bits
+            double real2 = 1.79769313486232e307;//64bits
+            decimal real3 = 79228162514264337593543950335m;//128bits
+
+
+            // String e char
+            char letra = 'A';
+            char letra2 = '\u0041';
+            string palavra = "Olá tudo bem?";
+            string palavra2 = null;
+
+            // Booleanos
+            bool booleano = true;
+
+            // Var
+            // O tipo é definido de acordo com o dado que entra nele
+            // var variante1;//Não posso declarar sem valor
+            var variante2 = "Eae";
+            // var variante2 = 1; // Não posso mudar de valor
+            var variante3 = 123;
+
+
+            // Object
+            // Valor padrão null, é a base de todos os tipos
+            // onde podemos atribuir qualquer valor
+            object obj = 123;
+            obj = 123.12f;
+            obj = 'A';
+            obj = "123";
+            obj = true;
+
+
+            // Constantes
+            const string constanteString = "NÃO DÁ PRA ALTERAR";
+            const int constanteInt = 123;
+
+
+            // Atribuindo valores ao struct
+            Pessoa p2 = new Pessoa()
+            {
+                nome = "Lucas",
+                idade = num3,
+            };
+            Pessoa p1 = new Pessoa();
+            p1.nome = "Iago";
+            p1.idade = 20;
+
+            // p1.altura não pode ser atribuida pois é private
+            #endregion
+
+            #region Conversões de Tipo
+            // Casting, pode ocorrer perda de dados
+            float numeroFloat = 257.232f;
+            int numeroInt = (int)numeroFloat; // Perde casas decimais
+            byte numeroByte = (byte)numeroInt; // Converte para abrangencia dos bytes (pega o modulo do tamanho suportado)
+            char letraChar = (char)97; // Converte para char conforme tabela ASCII
+
+            Console.WriteLine("Numero convertido int = " + numeroInt);
+            Console.WriteLine("Numero convertido byte = " + numeroByte);
+            Console.WriteLine("Numero convertido char = " + letraChar);
+            
+
+            // Parse String -> Numeros
+            numeroInt = int.Parse("1986");
+            numeroByte = byte.Parse("128");
+            numeroFloat = float.Parse("1,12");
+
+
+            // Classe Convert -> Converte qualquer tipo para qualquer outro tipo
+            // Cuidado que alguns valores não podem ser convertidos para outros
+            Convert.ToDecimal("2232");
+            Convert.ToDouble(2232.54435);
+            Convert.ToBoolean(12);
+            Convert.ToString(true);
+            Convert.ToUInt64('1');
+            #endregion
+
+            #region Operadores, Incremento
+            // Operadores + - / * %
+            int incremento = 0, decremento = 0;
+            Console.WriteLine(incremento++); // 0 
+            Console.WriteLine(++incremento); // 2
+            Console.WriteLine(decremento--); // 0
+            Console.WriteLine(--decremento); // -2
+
+
+            // Operadoder de atribuição
+            int valor;
+            valor = 10; // 10
+            valor += 10; // 20
+            valor -= 10; // 10
+            valor /= 10; // 1
+            valor *= 10; // 10
+            valor %= 10; // 10
+
+            // Operadores de igualdade == !=
+
+            // Operadores relacionais > < >= <=
+
+            // Operadores Lógicos && || & |
+            /*
+             * if(condição1 || condição2 || condição3)
+             * Se a condição1 for verdadeira, as condições 2 e 3 NÃO serão verificadas.
+             * if(condição1 | condição2 | condição3)
+             * Isso verificará as condições 2 e 3, mesmo que 1 já seja verdadeiro. 
+             *  aumento de desempenho usando-as.
+            */
+#endregion
+
+            #region Vetores e Matrizes
+            int[] vetor = new int[3];
+            int[] vetor2 = { 11, 22, 33 };
+            vetor[0] = 10;
+            vetor[1] = 20;
+            vetor[2] = 30;
+
+            foreach (int i in vetor)
+            {
+                Console.WriteLine(i);
+            }
+
+            // MATRIZ
+            int[,] matriz = new int[2, 3];
+            int[,] matriz2 = {  {00, 01, 02 },
+                                {10, 11, 12 },
+                                {20, 21, 22 },
+            };
+            Console.WriteLine(matriz2[1, 1]);
+
+            int[,,] matriz3 = {
+                                {{000, 001, 002 }, {010, 011, 012 }},
+                                {{100, 101, 102 }, {110, 111, 112 }},
+                                {{200, 201, 202 }, {210, 211, 212 }},
+            };
+            Console.WriteLine(matriz3[1, 1, 1]);
+
+            #endregion
+
+            #region Estruturas Condicionais, Operador Ternario e Switch
+            if (0 == 1){
+            } else if (1 != 2){
+            } else{
+            }
+
+
+            string frase = 1 > 0 ? "Condição Verdadeira" : "Condição Falsa";
+            Console.WriteLine(frase);
+
+            numeroInt = 0 | 4;
+            switch (numeroInt)
+            {
+                case 0:
+                    break;
+                case 1 | 2:
+                    Console.WriteLine("1 ou 2");
+                    break;
+                case 4:
+                case 5:
+                    Console.WriteLine("4 ou 5");
+                    break;
+                default:
+                    Console.WriteLine("Opção defaulr");   
+                    break;
+            }
+            #endregion
+
+            #region GOTO
+            goto mensagem; 
+            Console.WriteLine("Código Pulado");
+
+        mensagem:
+            Console.WriteLine("Assim funciona o GOTO");
+            #endregion
+
+            #region Estruturas de Repetição
+            int contador = 0;
+            while(contador <= 3)
+            {
+                Console.Write(contador);
+                contador++;
+            }
+
+            do
+            {
+                Console.WriteLine("\nNão faz a primeira verificação");
+            } while (false);
+
+
+            for (int i = 0, j = 10; i < 10; i++, j--)
+            {
+                if (i == j)
+                {
+                    break;
+                } 
+                Console.WriteLine(i + " <--> " + j);
+            }
+
+            foreach(int i in vetor)
+            {
+                Console.WriteLine(i);
+            }
+            #endregion
+
+            #region Entrada e saida de dados
+            Console.Write("Não quebra a linha");
+            Console.WriteLine("Quebra a linha");
+
+            string texto = Console.ReadLine(); //Lê a linha inteira
+            Console.WriteLine(texto);
+
+            int codigo = Console.Read(); // Lê 1 caractere(ASCII) e espera apertar a tecla ENTER
+            Console.Write(codigo);
+
+            Console.ReadKey();//Lê 1 caractere(ASCII), não espera apertar a tecla ENTER
+            Console.Clear(); // Limpa o console para não pular 
+            #endregion
+        }
+
+    }
+}
